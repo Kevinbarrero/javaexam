acc1e1c8c4bb64d78c5f8436c07fd77883d476db
Kevinbarrero <kevinbar98@gmail.com>
2022-06-25 11:53:33 +0300

    first commit

---
+++ src/test/java/ru/itis/examjava/ExamJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.examjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
